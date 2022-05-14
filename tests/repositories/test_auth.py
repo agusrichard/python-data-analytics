e8ccc4a788cb10b6fc7d3780b5fb5733cd38faae
Agus Richard Lubis <51517401+agusrichard@users.noreply.github.com>
2022-05-14 16:00:12 +0700

    Feat/auth repository follow (#4)
    * Write tests, add new column avatar, and define followed-followers relationship
    
    * Add new functionalities to user repository regarding followers

--- tests/repositories/test_auth.py
+++ tests/repositories/test_auth.py
@@ -1,3 +1,4 @@
+import pytest
 from unittest import mock
 from datetime import datetime
 
@@ -6,10 +7,15 @@ from app.repositories.auth import AuthRepository
 TEST_EMAIL = "test@test.com"
 
 
-@mock.patch("app.db")
-@mock.patch("app.repositories.auth.User")
-def test_create_user(MockedUser, mocked_db):
-    auth_repository = AuthRepository(mocked_db)
+@pytest.fixture
+def auth_repository_db_user():
+    with mock.patch("app.repositories.auth.SQLAlchemy") as mocked_db:
+        with mock.patch("app.repositories.auth.User") as MockedUser:
+            yield AuthRepository(mocked_db), mocked_db, MockedUser
+
+
+def test_positive_create_user(auth_repository_db_user):
+    auth_repository, mocked_db, MockedUser = auth_repository_db_user
 
     data = {
         "username": "test",
@@ -23,16 +29,49 @@ def test_create_user(MockedUser, mocked_db):
     MockedUser.from_dict.assert_called_once_with(data)
 
 
-@mock.patch("app.db")
-@mock.patch("app.repositories.auth.User")
-def test_update_user(MockedUser, mocked_db):
-    auth_repository = AuthRepository(mocked_db)
+def test_positive_get_user_by_id(auth_repository_db_user):
+    auth_repository, _, MockedUser = auth_repository_db_user
+
+    auth_repository.get_by_id(1)
+
+    MockedUser.query.get.assert_called_once_with(1)
+
+
+def test_negative_get_user_by_id_not_found(auth_repository_db_user):
+    auth_repository, _, MockedUser = auth_repository_db_user
+    MockedUser.query.get.return_value = None
+
+    assert auth_repository.get_by_id(1) is None
+    MockedUser.query.get.assert_called_once_with(1)
+
+
+def test_positive_get_user_by_email(auth_repository_db_user):
+    auth_repository, _, MockedUser = auth_repository_db_user
+
+    auth_repository.get_by_email(TEST_EMAIL)
+
+    MockedUser.query.filter_by.assert_called_once_with(email=TEST_EMAIL)
+
+
+def test_negative_get_user_by_email_not_found(auth_repository_db_user):
+    auth_repository, _, MockedUser = auth_repository_db_user
+    MockedUser.query.filter_by.return_value.first.return_value = None
+
+    assert auth_repository.get_by_email(TEST_EMAIL) is None
+    MockedUser.query.filter_by.assert_called_once_with(email=TEST_EMAIL)
+    MockedUser.query.filter_by.return_value.first.assert_called_once()
+
+
+def test_positive_update_user(auth_repository_db_user):
+    auth_repository, mocked_db, MockedUser = auth_repository_db_user
+
     data = {
         "username": "test",
         "email": TEST_EMAIL,
         "fullname": "test",
         "bio": "test",
         "last_login": datetime.utcnow(),
+        "avatar": "test",
     }
     auth_repository.update(1, data)
 
@@ -40,38 +79,63 @@ def test_update_user(MockedUser, mocked_db):
     MockedUser.query.get.assert_called_once_with(1)
 
 
-@mock.patch("app.db")
-@mock.patch("app.repositories.auth.User")
-def test_update_user_not_found(MockedUser, mocked_db):
+def test_negative_update_user_not_found(auth_repository_db_user):
+    auth_repository, mocked_db, MockedUser = auth_repository_db_user
     MockedUser.query.get.return_value = None
 
-    auth_repository = AuthRepository(mocked_db)
     data = {
         "username": "test",
         "email": TEST_EMAIL,
         "fullname": "test",
         "bio": "test",
         "last_login": datetime.utcnow(),
+        "avatar": "test",
     }
     auth_repository.update(1, data)
 
+    mocked_db.session.commit.assert_not_called()
     MockedUser.query.get.assert_called_once_with(1)
-    assert MockedUser.query.get(1) is None
 
 
-@mock.patch("app.db")
-@mock.patch("app.repositories.auth.User")
-def test_get_user_by_email(MockedUser, mocked_db):
-    auth_repository = AuthRepository(mocked_db)
-    auth_repository.get_by_email(TEST_EMAIL)
+def test_positive_get_all_users(auth_repository_db_user):
+    auth_repository, _, MockedUser = auth_repository_db_user
 
-    MockedUser.query.filter_by.assert_called_once_with(email=TEST_EMAIL)
-
-
-@mock.patch("app.db")
-@mock.patch("app.repositories.auth.User")
-def test_get_all_users(MockedUser, mocked_db):
-    auth_repository = AuthRepository(mocked_db)
     auth_repository.get_all()
 
     MockedUser.query.limit.assert_called_once()
+    MockedUser.query.limit.return_value.offset.assert_called_once()
+    MockedUser.query.limit.return_value.offset.return_value.all.assert_called_once()
+
+
+def test_positive_get_all_users_with_take_skip(auth_repository_db_user):
+    auth_repository, _, MockedUser = auth_repository_db_user
+
+    auth_repository.get_all(take=10, skip=10)
+
+    MockedUser.query.limit.assert_called_once_with(10)
+    MockedUser.query.limit.return_value.offset.assert_called_once_with(10)
+    MockedUser.query.limit.return_value.offset.return_value.all.assert_called_once()
+
+
+def test_positive_follow_user(auth_repository_db_user):
+    auth_repository, mocked_db, MockedUser = auth_repository_db_user
+    user = MockedUser()
+    user1 = MockedUser()
+
+    auth_repository.follow(user, user1)
+
+    mocked_db.session.commit.assert_called_once()
+    user.follow.assert_called_once_with(user1)
+
+
+def test_positive_unfollow_user(auth_repository_db_user):
+    auth_repository, mocked_db, MockedUser = auth_repository_db_user
+    user = MockedUser()
+    user1 = MockedUser()
+
+    auth_repository.follow(user, user1)
+    auth_repository.unfollow(user, user1)
+
+    user.follow.assert_called_once_with(user1)
+    user.unfollow.assert_called_once_with(user1)
+    assert mocked_db.session.commit.call_count == 2
