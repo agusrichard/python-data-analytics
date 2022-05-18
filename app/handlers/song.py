9c8e44ca5a6977a387fa9fc21a7a42cc31157778
Agus Richard Lubis <agus.richard21@gmail.com>
2022-05-18 13:56:43 +0700

    Working on Song handlers

---
+++ app/handlers/song.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, request
+
+from app.common.token import token_required
+from app.controllers.song import SongController
+
+
+def create_song_handlers(controller: SongController):
+    blueprint = Blueprint("song", __name__)
+
+    @blueprint.route("/create", methods=["POST"])
+    @token_required
+    def create():
+        return controller.create(request)
+
+    @blueprint.route("/update", methods=["POST"])
+    @token_required
+    def update():
+        return controller.update(request)
+
+    @blueprint.route("/delete", methods=["POST"])
+    @token_required
+    def delete():
+        return controller.delete(request)
+
+    @blueprint.route("/get-all", methods=["GET"])
+    @token_required
+    def get_all():
+        return controller.get_all(request)
+
+    @blueprint.route("/get-by-id", methods=["GET"])
+    @token_required
+    def get_by_id():
+        return controller.get_by_id(request)
+
+    return blueprint
