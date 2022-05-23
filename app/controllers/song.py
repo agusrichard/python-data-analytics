44e0b06d8f9a832e4a8d1a4d61ba0f27805fe92e
Agus Richard Lubis <agus.richard21@gmail.com>
2022-05-23 16:11:19 +0700

    Working on Song controllers and handlers

--- app/controllers/song.py
+++ app/controllers/song.py
@@ -5,6 +5,13 @@ from flask import Response, Request, jsonify
 from app.models.song import Song
 from app.models.user import User
 from app.services.song import SongService
+from app.common.exceptions import (
+    BadRequestException,
+    FieldRequired,
+    Unauthorized,
+    UploadFailedException,
+    NotFoundException,
+)
 
 
 class SongController:
@@ -14,39 +21,61 @@ class SongController:
     def create(self, request: Request, current_user: User) -> Tuple[Response, int]:
         try:
             files = {
-                "song_file": request.files["song_file"],
-                "small_thumbnail_file": request.files["small_thumbnail_file"],
-                "large_thumbnail_file": request.files["large_thumbnail_file"],
+                "song_file": request.files.get("song_file", None),
+                "small_thumbnail_file": request.files.get("small_thumbnail_file", None),
+                "large_thumbnail_file": request.files.get("large_thumbnail_file", None),
             }
             data = request.form.to_dict()
             data["user_id"] = current_user.id
+            print("create song files", files)
+            print("create song data", data)
             self.service.create(files, data)
-            return "", HTTPStatus.OK
-        except Exception:
-            return "", HTTPStatus.INTERNAL_SERVER_ERROR
+            return "", HTTPStatus.CREATED
+        except FieldRequired as e:
+            return jsonify(e.to_dict()), e.error_code
+        except UploadFailedException as e:
+            return jsonify(e.to_dict()), e.error_code
+        except BadRequestException as e:
+            return jsonify(e.to_dict()), e.error_code
 
-    def update(self, song_id: Optional[int], request: Request) -> Tuple[Response, int]:
+    def update(
+        self, current_user: User, song_id: Optional[int], request: Request
+    ) -> Tuple[Response, int]:
         try:
             if song_id is None:
-                return "", HTTPStatus.BAD_REQUEST
+                err = FieldRequired("song_id")
+                return err.to_dict(), err.error_code
 
             song_id = int(song_id)
             files = {}
             for key, file in request.files.items():
                 files[key] = file
-            song = self.service.update(song_id, files, request.form.to_dict())
-            return jsonify(song), HTTPStatus.OK
-        except Exception as e:
-            print("Exception:", e)
-            return "", HTTPStatus.INTERNAL_SERVER_ERROR
 
-    def delete(self, request: Request) -> Tuple[Response, int]:
-        try:
-            song_id = int(request.view_args["song_id"])
-            self.service.delete(song_id)
+            self.service.update(current_user, song_id, files, request.form.to_dict())
             return "", HTTPStatus.OK
-        except Exception as e:
-            return "", HTTPStatus.INTERNAL_SERVER_ERROR
+        except Unauthorized as e:
+            return jsonify(e.to_dict()), e.error_code
+        except NotFoundException as e:
+            return jsonify(e.to_dict()), e.error_code
+        except UploadFailedException as e:
+            return jsonify(e.to_dict()), e.error_code
+        except BadRequestException as e:
+            return jsonify(e.to_dict()), e.error_code
+
+    def delete(
+        self, current_user: User, song_id: Optional[int]
+    ) -> Tuple[Response, int]:
+        try:
+            if song_id is None:
+                err = FieldRequired("song_id")
+                return err.to_dict(), err.error_code
+
+            self.service.delete(current_user, song_id)
+            return "", HTTPStatus.OK
+        except Unauthorized as e:
+            return jsonify(e.to_dict()), e.error_code
+        except NotFoundException as e:
+            return jsonify(e.to_dict()), e.error_code
 
     def get_all(self, request: Request) -> Tuple[List[Song], int]:
         take = request.args.get("take", 10, int)
@@ -57,10 +86,9 @@ class SongController:
             HTTPStatus.OK,
         )
 
-    def get_by_id(self, request: Request) -> Tuple[Song, int]:
-        song_id = int(request.view_args["song_id"])
-
-        return (
-            jsonify(self.service.get_by_id(song_id)),
-            HTTPStatus.OK,
-        )
+    def get_by_id(self, song_id: Optional[int]) -> Tuple[Song, int]:
+        try:
+            song = self.service.get_by_id(song_id)
+            return jsonify(song), HTTPStatus.OK
+        except NotFoundException as e:
+            return jsonify(e.to_dict()), e.error_code
